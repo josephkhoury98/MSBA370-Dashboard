7c6d9f4bb83f66d02b2d96ce46f38713d1d987d5
josephkhoury98 <79418576+josephkhoury98@users.noreply.github.com>
2021-05-18 13:20:59 +0300

    Update Sales_New.py

--- Sales_New.py
+++ Sales_New.py
@@ -21,10 +21,10 @@ import matplotlib.image as mpimg
 
 #------------------------------ Data collection ---------------------------------
 sales = pd.read_csv('https://github.com/josephkhoury98/MSBA370-Dashboard/blob/main/SALES_SALES.csv',error_bad_lines=False)
-sales = sales.rename({'Sales Category': 'Sales_Category'}, axis=1)
+sales = sales.rename({'Sales Category': 'sales_cat'}, axis=1)
 sales.info()
 rent = pd.read_csv('https://github.com/josephkhoury98/MSBA370-Dashboard/blob/main/RENT_RENT.csv', error_bad_lines=False)
-rent = rent.rename({'Sales Category': 'Sales_Category'}, axis=1)
+rent = rent.rename({'Sales Category': 'sales_cat'}, axis=1)
 rent.info()
 
 missing_values_count = pd.DataFrame({'Null':sales.isnull().sum()})
@@ -72,11 +72,11 @@ st.markdown(
 
 col1, col2, col3 =st.beta_columns([47.5,5,47.5])
 #------------------------------------------Data per Category -----------------------------------------------------
-rent_category = rent.groupby(rent['Sales_Category']).sum()
+rent_category = rent.groupby(rent['sales_cat']).sum()
 rent_category = pd.DataFrame(rent_category.to_records())
-rent_category = rent_category.drop(['Sales_Category', 'GLA'], axis=1)
+rent_category = rent_category.drop(['sales_cat', 'GLA'], axis=1)
 rent_category = pd.DataFrame(rent_category.to_records())
-sales_category = sales.groupby(sales['Sales_Category']).sum()
+sales_category = sales.groupby(sales['sales_cat']).sum()
 sales_category = pd.DataFrame(sales_category.to_records())
 sales_category = pd.concat([sales_category, rent_category.reindex(sales_category.index)], axis=1)
 sales_category['Sales by SQM'] = sales_category['Sales']/sales_category['GLA']
@@ -93,8 +93,8 @@ missing_cat_values_count.sort_values(by='Null', ascending = False)
 #No missing values thus no need to impute or do anything
 
 rent_sales_persqm = make_subplots(specs=[[{"secondary_y": True}]])
-rent_sales_persqm.add_trace(go.Scatter(x=sales_category['Sales_Category'], y=sales_category['Rent by SQM'], name = 'Rent per SQM'),secondary_y=True,)
-rent_sales_persqm.add_trace(go.Bar(x=sales_category['Sales_Category'], y=sales_category['Sales by SQM'], name= "Categories"),secondary_y=False,)
+rent_sales_persqm.add_trace(go.Scatter(x=sales_category['sales_cat'], y=sales_category['Rent by SQM'], name = 'Rent per SQM'),secondary_y=True,)
+rent_sales_persqm.add_trace(go.Bar(x=sales_category['sales_cat'], y=sales_category['Sales by SQM'], name= "Categories"),secondary_y=False,)
 #rent_sales_persqm.update_layout(title_text="Double Y Axis Example")
 rent_sales_persqm.update_layout(xaxis={'visible': False, 'showticklabels': False})
 rent_sales_persqm.update_layout(template="simple_white")
@@ -108,7 +108,7 @@ rent_sales_persqm.update_yaxes(title_text="<b>Sales </b>per SQM", secondary_y=Fa
 #----------------------------------------------------------------------------------------------------------
 
 sales_area = px.scatter(sales_category,x= sales_category['GLA'],y= sales_category['Sales'], trendline='ols', trendline_color_override = 'lightgrey' ,
- template="simple_white", labels={'GLA' : 'Area (in SQM)'}, hover_name=sales_category['Sales_Category'] )
+ template="simple_white", labels={'GLA' : 'Area (in SQM)'}, hover_name=sales_category['sales_cat'] )
 #----------------------------------------------------------------------------------------------------------
 
 #----------------------------------------------------------------------------------------------------------
@@ -186,7 +186,7 @@ col3.plotly_chart (sales_area)
 #--------------------------------------Prediction of Sales------------------------
 pred_data = sales_category
 pred_data = pred_data.drop('index', axis=1)
-categorical_df = pred_data[['Sales_Category']]
+categorical_df = pred_data[['sales_cat']]
 numerical_df = pred_data[['GLA', 'Sales', 'Rent', 'Rent by SQM']]
 dummy = pd.get_dummies(categorical_df)
 encoded_df= pd.concat ([numerical_df, dummy], axis=1)
@@ -218,113 +218,113 @@ cat=col1.selectbox('Select a Category:',('Arabian Perfumes','Cafes','Childrens F
 'Jewellery','Lingerie & Swimwear', 'Luggage', 'Mens Fashion', 'Mobile Phone & Accessories','Music', 'Optometrist & Sun Glasses','Perfumes & Cosmetics', 'Pharmacy', 'Restaurants',
 'Services', 'Speciality Food','Sports & Leisure','Toys & Hobbies','Unisex Fashion','Watches'))
 
-Sales_Category_ArabianPerfumes = 0
-Sales_Category_Cafes = 0
-Sales_Category_ChildrensFashion = 0
-Sales_Category_Cinema = 0
-Sales_Category_DepartmentStores = 0
-Sales_Category_Electronics_Gadgets_Accessories = 0
-Sales_Category_Entertainment = 0
-Sales_Category_Entertainment_Other = 0
-Sales_Category_FashionAccessories = 0
-Sales_Category_FemaleFashion = 0
-Sales_Category_FoodCourt = 0
-Sales_Category_Footwear = 0
-Sales_Category_GeneralStore = 0
-Sales_Category_Health_Nutrition = 0
-Sales_Category_HomeFurnishings = 0
-Sales_Category_Hypermarket = 0
-Sales_Category_Jewellery = 0
-Sales_Category_Lingerie_Swimwear = 0
-Sales_Category_Luggage = 0
-Sales_Category_MensFashion = 0
-Sales_Category_MobilePhone_Accessories = 0
-Sales_Category_Music = 0
-Sales_Category_Optometrist_SunGlasses = 0
-Sales_Category_Perfumes_Cosmetics = 0
-Sales_Category_Pharmacy = 0
-Sales_Category_Restaurants = 0
-Sales_Category_Services = 0
-Sales_Category_SpecialityFood = 0
-Sales_Category_Sports_Leisure = 0
-Sales_Category_Toys_Hobbies = 0
-Sales_Category_UnisexFashion = 0
-Sales_Category_Watches = 0
+sales_cat_ArabianPerfumes = 0
+sales_cat_Cafes = 0
+sales_cat_ChildrensFashion = 0
+sales_cat_Cinema = 0
+sales_cat_DepartmentStores = 0
+sales_cat_Electronics_Gadgets_Accessories = 0
+sales_cat_Entertainment = 0
+sales_cat_Entertainment_Other = 0
+sales_cat_FashionAccessories = 0
+sales_cat_FemaleFashion = 0
+sales_cat_FoodCourt = 0
+sales_cat_Footwear = 0
+sales_cat_GeneralStore = 0
+sales_cat_Health_Nutrition = 0
+sales_cat_HomeFurnishings = 0
+sales_cat_Hypermarket = 0
+sales_cat_Jewellery = 0
+sales_cat_Lingerie_Swimwear = 0
+sales_cat_Luggage = 0
+sales_cat_MensFashion = 0
+sales_cat_MobilePhone_Accessories = 0
+sales_cat_Music = 0
+sales_cat_Optometrist_SunGlasses = 0
+sales_cat_Perfumes_Cosmetics = 0
+sales_cat_Pharmacy = 0
+sales_cat_Restaurants = 0
+sales_cat_Services = 0
+sales_cat_SpecialityFood = 0
+sales_cat_Sports_Leisure = 0
+sales_cat_Toys_Hobbies = 0
+sales_cat_UnisexFashion = 0
+sales_cat_Watches = 0
 if cat == 'Arabian Perfumes':
-    Sales_Category_ArabianPerfumes = 1
+    sales_cat_ArabianPerfumes = 1
 elif cat == 'Cafes':
-    Sales_Category_Cafes = 1
+    sales_cat_Cafes = 1
 elif cat == 'Childrens Fashion': 
-    Sales_Category_ChildrensFashion = 1
+    sales_cat_ChildrensFashion = 1
 elif cat == 'Cinema':
-    Sales_Category_Cinema = 1
+    sales_cat_Cinema = 1
 elif cat == 'Department Stores':
-    Sales_Category_DepartmentStores = 1
+    sales_cat_DepartmentStores = 1
 elif cat == 'Electronics & Gadgets & Accessories':
-    Sales_Category_Electronics_Gadgets_Accessories = 1
+    sales_cat_Electronics_Gadgets_Accessories = 1
 elif cat == 'Entertainment':
-    Sales_Category_Entertainment = 1
+    sales_cat_Entertainment = 1
 elif cat == 'Entertainment - Other':
-    Sales_Category_Entertainment_Other = 1
+    sales_cat_Entertainment_Other = 1
 elif cat == 'Fashion Accessories':
-    Sales_Category_FashionAccessories = 1
+    sales_cat_FashionAccessories = 1
 elif cat == 'Female Fashion':
-    Sales_Category_FemaleFashion = 1
+    sales_cat_FemaleFashion = 1
 elif cat == 'Food Court':
-    Sales_Category_FoodCourt = 1
+    sales_cat_FoodCourt = 1
 elif cat == 'Footwear':
-    Sales_Category_Footwear = 1
+    sales_cat_Footwear = 1
 elif cat == 'General Store':
-    Sales_Category_GeneralStore = 1
+    sales_cat_GeneralStore = 1
 elif cat == 'Health & Nutrition':
-    Sales_Category_Health_Nutrition = 1
+    sales_cat_Health_Nutrition = 1
 elif cat == 'Home Furnishings':
-    Sales_Category_HomeFurnishings = 1
+    sales_cat_HomeFurnishings = 1
 elif cat == 'Hypermarket':
-    Sales_Category_Hypermarket = 1
+    sales_cat_Hypermarket = 1
 elif cat == 'Jewellery':
-    Sales_Category_Jewellery = 1
+    sales_cat_Jewellery = 1
 elif cat == 'Lingerie & Swimwear':
-    Sales_Category_Lingerie_Swimwear = 1
+    sales_cat_Lingerie_Swimwear = 1
 elif cat == 'Luggage':
-    Sales_Category_Luggage = 1
+    sales_cat_Luggage = 1
 elif cat == 'Mens Fashion':
-    Sales_Category_MensFashion = 1
+    sales_cat_MensFashion = 1
 elif cat == 'Mobile Phone & Accessories':
-    Sales_Category_MobilePhone_Accessories = 1
+    sales_cat_MobilePhone_Accessories = 1
 elif cat == 'Music':
-    Sales_Category_Music = 1
+    sales_cat_Music = 1
 elif cat == 'Optometrist & Sun Glassess':
-    Sales_Category_Optometrist_SunGlasses = 1
+    sales_cat_Optometrist_SunGlasses = 1
 elif cat == 'Perfumes & Cosmetics':
-    Sales_Category_Perfumes_Cosmetics = 1
+    sales_cat_Perfumes_Cosmetics = 1
 elif cat == 'Pharmacy':
-    Sales_Category_Pharmacy = 1
+    sales_cat_Pharmacy = 1
 elif cat == 'Restaurants':
-    Sales_Category_Restaurants = 1
+    sales_cat_Restaurants = 1
 elif cat == 'Services':
-    Sales_Category_Services = 1
+    sales_cat_Services = 1
 elif cat == 'Speciality Food':
-    Sales_Category_SpecialityFood = 1
+    sales_cat_SpecialityFood = 1
 elif cat == 'Sports & Leisure':
-    Sales_Category_Sports_Leisure = 1
+    sales_cat_Sports_Leisure = 1
 elif cat == 'Toys & Hobbies':
-    Sales_Category_Toys_Hobbies = 1
+    sales_cat_Toys_Hobbies = 1
 elif cat == 'Unisex Fashion':
-    Sales_Category_UnisexFashion = 1
+    sales_cat_UnisexFashion = 1
 elif cat == 'Watches':
-    Sales_Category_Watches = 1
+    sales_cat_Watches = 1
 
 
 
 GLA_input = col3.text_input("Category Surface Area", "Type here")
 Rent_input = col1.text_input("Rent per Category", "Type here")
 RentBySQM = col3.text_input("Rent per SQM", "Type here")
-def predict_sales(GLA, Rent, RentBySQM,Sales_Category_ArabianPerfumes,Sales_Category_Cafes,Sales_Category_ChildrensFashion,Sales_Category_Cinema,Sales_Category_DepartmentStores,Sales_Category_Electronics_Gadgets_Accessories,Sales_Category_Entertainment,Sales_Category_Entertainment_Other,Sales_Category_FashionAccessories,Sales_Category_FemaleFashion,Sales_Category_FoodCourt,Sales_Category_Footwear,Sales_Category_GeneralStore,Sales_Category_Health_Nutrition,Sales_Category_HomeFurnishings,Sales_Category_Hypermarket,Sales_Category_Jewellery,Sales_Category_Lingerie_Swimwear,Sales_Category_Luggage,Sales_Category_MensFashion,Sales_Category_MobilePhone_Accessories,Sales_Category_Music,Sales_Category_Optometrist_SunGlasses,Sales_Category_Perfumes_Cosmetics,Sales_Category_Pharmacy,Sales_Category_Restaurants,Sales_Category_Services,Sales_Category_SpecialityFood,Sales_Category_Sports_Leisure,Sales_Category_Toys_Hobbies,Sales_Category_UnisexFashion,Sales_Category_Watches):
-    prediction = linReg.predict([[GLA, Rent, RentBySQM,Sales_Category_ArabianPerfumes,Sales_Category_Cafes,Sales_Category_ChildrensFashion,Sales_Category_Cinema,Sales_Category_DepartmentStores,Sales_Category_Electronics_Gadgets_Accessories,Sales_Category_Entertainment,Sales_Category_Entertainment_Other,Sales_Category_FashionAccessories,Sales_Category_FemaleFashion,Sales_Category_FoodCourt,Sales_Category_Footwear,Sales_Category_GeneralStore,Sales_Category_Health_Nutrition,Sales_Category_HomeFurnishings,Sales_Category_Hypermarket,Sales_Category_Jewellery,Sales_Category_Lingerie_Swimwear,Sales_Category_Luggage,Sales_Category_MensFashion,Sales_Category_MobilePhone_Accessories,Sales_Category_Music,Sales_Category_Optometrist_SunGlasses,Sales_Category_Perfumes_Cosmetics,Sales_Category_Pharmacy,Sales_Category_Restaurants,Sales_Category_Services,Sales_Category_SpecialityFood,Sales_Category_Sports_Leisure,Sales_Category_Toys_Hobbies,Sales_Category_UnisexFashion,Sales_Category_Watches]])
+def predict_sales(GLA, Rent, RentBySQM,sales_cat_ArabianPerfumes,sales_cat_Cafes,sales_cat_ChildrensFashion,sales_cat_Cinema,sales_cat_DepartmentStores,sales_cat_Electronics_Gadgets_Accessories,sales_cat_Entertainment,sales_cat_Entertainment_Other,sales_cat_FashionAccessories,sales_cat_FemaleFashion,sales_cat_FoodCourt,sales_cat_Footwear,sales_cat_GeneralStore,sales_cat_Health_Nutrition,sales_cat_HomeFurnishings,sales_cat_Hypermarket,sales_cat_Jewellery,sales_cat_Lingerie_Swimwear,sales_cat_Luggage,sales_cat_MensFashion,sales_cat_MobilePhone_Accessories,sales_cat_Music,sales_cat_Optometrist_SunGlasses,sales_cat_Perfumes_Cosmetics,sales_cat_Pharmacy,sales_cat_Restaurants,sales_cat_Services,sales_cat_SpecialityFood,sales_cat_Sports_Leisure,sales_cat_Toys_Hobbies,sales_cat_UnisexFashion,sales_cat_Watches):
+    prediction = linReg.predict([[GLA, Rent, RentBySQM,sales_cat_ArabianPerfumes,sales_cat_Cafes,sales_cat_ChildrensFashion,sales_cat_Cinema,sales_cat_DepartmentStores,sales_cat_Electronics_Gadgets_Accessories,sales_cat_Entertainment,sales_cat_Entertainment_Other,sales_cat_FashionAccessories,sales_cat_FemaleFashion,sales_cat_FoodCourt,sales_cat_Footwear,sales_cat_GeneralStore,sales_cat_Health_Nutrition,sales_cat_HomeFurnishings,sales_cat_Hypermarket,sales_cat_Jewellery,sales_cat_Lingerie_Swimwear,sales_cat_Luggage,sales_cat_MensFashion,sales_cat_MobilePhone_Accessories,sales_cat_Music,sales_cat_Optometrist_SunGlasses,sales_cat_Perfumes_Cosmetics,sales_cat_Pharmacy,sales_cat_Restaurants,sales_cat_Services,sales_cat_SpecialityFood,sales_cat_Sports_Leisure,sales_cat_Toys_Hobbies,sales_cat_UnisexFashion,sales_cat_Watches]])
     return prediction
 
 results=""
 if col1.button('Predict'):
-    results = predict_sales(GLA_input, Rent_input, RentBySQM,Sales_Category_ArabianPerfumes,Sales_Category_Cafes,Sales_Category_ChildrensFashion,Sales_Category_Cinema,Sales_Category_DepartmentStores,Sales_Category_Electronics_Gadgets_Accessories,Sales_Category_Entertainment,Sales_Category_Entertainment_Other,Sales_Category_FashionAccessories,Sales_Category_FemaleFashion,Sales_Category_FoodCourt,Sales_Category_Footwear,Sales_Category_GeneralStore,Sales_Category_Health_Nutrition,Sales_Category_HomeFurnishings,Sales_Category_Hypermarket,Sales_Category_Jewellery,Sales_Category_Lingerie_Swimwear,Sales_Category_Luggage,Sales_Category_MensFashion,Sales_Category_MobilePhone_Accessories,Sales_Category_Music,Sales_Category_Optometrist_SunGlasses,Sales_Category_Perfumes_Cosmetics,Sales_Category_Pharmacy,Sales_Category_Restaurants,Sales_Category_Services,Sales_Category_SpecialityFood,Sales_Category_Sports_Leisure,Sales_Category_Toys_Hobbies,Sales_Category_UnisexFashion,Sales_Category_Watches )
+    results = predict_sales(GLA_input, Rent_input, RentBySQM,sales_cat_ArabianPerfumes,sales_cat_Cafes,sales_cat_ChildrensFashion,sales_cat_Cinema,sales_cat_DepartmentStores,sales_cat_Electronics_Gadgets_Accessories,sales_cat_Entertainment,sales_cat_Entertainment_Other,sales_cat_FashionAccessories,sales_cat_FemaleFashion,sales_cat_FoodCourt,sales_cat_Footwear,sales_cat_GeneralStore,sales_cat_Health_Nutrition,sales_cat_HomeFurnishings,sales_cat_Hypermarket,sales_cat_Jewellery,sales_cat_Lingerie_Swimwear,sales_cat_Luggage,sales_cat_MensFashion,sales_cat_MobilePhone_Accessories,sales_cat_Music,sales_cat_Optometrist_SunGlasses,sales_cat_Perfumes_Cosmetics,sales_cat_Pharmacy,sales_cat_Restaurants,sales_cat_Services,sales_cat_SpecialityFood,sales_cat_Sports_Leisure,sales_cat_Toys_Hobbies,sales_cat_UnisexFashion,sales_cat_Watches )
     col1.success('In the next 2 years, {} category will have ${} of sales '.format(cat, results))
