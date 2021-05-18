97a77b1aeac50ac4d48e537f975b1a7768014658
josephkhoury98 <79418576+josephkhoury98@users.noreply.github.com>
2021-05-18 14:32:55 +0300

    Update Sales_New.py

--- Sales_New.py
+++ Sales_New.py
@@ -20,10 +20,10 @@ from sklearn.metrics import mean_squared_error
 import matplotlib.image as mpimg
 
 #------------------------------ Data collection ---------------------------------
-sales = pd.read_csv('https://github.com/josephkhoury98/MSBA370-Dashboard/blob/main/SALES_SALES.csv',error_bad_lines=False)
+sales = pd.read_csv('https://raw.githubusercontent.com/josephkhoury98/MSBA370-Dashboard/main/SALES_SALES.csv',error_bad_lines=False)
 #sales = sales.rename({'Sales Category': 'Sales Category'}, axis=1)
 sales.info()
-rent = pd.read_csv('https://github.com/josephkhoury98/MSBA370-Dashboard/blob/main/RENT_RENT.csv', error_bad_lines=False)
+rent = pd.read_csv('https://raw.githubusercontent.com/josephkhoury98/MSBA370-Dashboard/main/RENT_RENT.csv', error_bad_lines=False)
 #rent = rent.rename({'Sales Category': 'Sales Category'}, axis=1)
 rent.info()
 
